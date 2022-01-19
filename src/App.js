f929a6407cc589a528e524203ba5b796d94f1866
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-01-18 20:43:40 -0500

    added NavBar + Home/Index pages w/ routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+//Dependencies
+import { Routes, Route } from "react-router-dom"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+//Components
+import NavBar from "./Components/NavBar"
+//Pages
+import Home from "./Pages/Home"
+import Index from "./Pages/Index"
+
+export default function App() {
+  return(
+    <div className="app">
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Home />}/>
+        <Route exact path="/transactions" element={<Index />}/>
+      </Routes>
     </div>
-  );
+  )
 }
-
-export default App;
