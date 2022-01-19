331ad562fafbb99cf9c7a984bd5d82e988bbd139
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-01-19 15:58:05 -0500

    added bootstrap Container/Stack

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,11 +1,14 @@
 //Dependencies
-import { Link } from "react-router-dom"
+import { Link  } from "react-router-dom"
+import { Button, Container , Stack} from "react-bootstrap"
 
 export default function NavBar() {
     return(
-        <div className="navbar">
-            <h1>Budget App</h1>
-            <button>New Transaction</button>
-        </div>
+        <Container className="my-4">
+            <Stack direction="horizontal" gap="2" className="mb-5">
+                <h1 className="me-auto">Budget App</h1>
+                <Button variant="primary">New Transaction</Button>
+            </Stack> 
+        </Container>
     )
 }
