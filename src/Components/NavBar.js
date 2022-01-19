93171dcb92c3f628738581efdff34820b8579ffc
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-01-18 20:44:09 -0500

    modified app

---
+++ src/Components/NavBar.js
@@ -0,0 +1,11 @@
+//Dependencies
+import { Link } from "react-router-dom"
+
+export default function NavBar() {
+    return(
+        <div className="navbar">
+            <h1>Budget App</h1>
+            <button>New Transaction</button>
+        </div>
+    )
+}
