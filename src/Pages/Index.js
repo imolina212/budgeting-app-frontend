67d9b103d2a8bb9f9aa42bfbfb3d5edc7a591a69
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-01-19 16:42:46 -0500

    added budget card

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -1,7 +1,16 @@
+import Container from "react-bootstrap/Container"
+import { Stack } from "react-bootstrap"
+import BudgetCard from "../Components/BudgetCard"
+
 export default function Index() {
     return(
-        <div>
-            <h1>Index</h1>
-        </div>
+        <Container>
+            <Stack direction="horizontal" gap="2" className="mb-4">
+                <h1 className="me-auto">Your current Budgets (Index)</h1>
+            </Stack>
+            <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", gap: "1rem", alignItems: "flex-start" }}>
+                <BudgetCard name="Food" amount={150} max={300} ></BudgetCard>
+            </div>
+        </Container>
     )
 }
