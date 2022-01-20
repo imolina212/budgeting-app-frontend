f44d056451576cd6f531e32dbc3cc9c91be4bf3d
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-01-20 11:05:38 -0500

    changed new transaction button color

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -6,11 +6,11 @@ export default function NavBar() {
     return(
         <Container className="my-4">
             <Stack direction="horizontal" gap="2" className="mb-5">
-                <Link to="/transactions/budget" style={{textDecoration: "none",}}>
-                    <h1 className="me-auto">Budget App</h1>
+                <Link to="/transactions/budget"  className="me-auto" style={{textDecoration: "none",}}>
+                    <h1>Budget App</h1>
                 </Link>
                 <Link to="/transactions/new">
-                    <Button variant="primary">New Transaction</Button>
+                    <Button variant="outline-primary">New Transaction</Button>
                 </Link>
                 <Link to="/transactions">
                     <Button variant="primary">View Transactions</Button>  
