d8923fbaca1ee448fa7c8169666038760dffa1bd
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-01-19 23:32:04 -0500

    added Transactions Component

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -1,16 +1,9 @@
-import Container from "react-bootstrap/Container"
-import { Stack } from "react-bootstrap"
-import BudgetCard from "../Components/BudgetCard"
+import Transactions from "../Components/Transactions";
 
 export default function Index() {
     return(
-        <Container>
-            <Stack direction="horizontal" gap="2" className="mb-4">
-                <h1 className="me-auto">Your current Budgets (Index)</h1>
-            </Stack>
-            <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", gap: "1rem", alignItems: "flex-start" }}>
-                <BudgetCard name="Food" amount={150} max={300} ></BudgetCard>
-            </div>
-        </Container>
+        <div>
+            <Transactions />
+        </div>
     )
 }
