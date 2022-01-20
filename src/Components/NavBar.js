b91803dc2d51ec011f3e6a2cde697a4e386aa43e
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-01-19 22:06:24 -0500

    added links to buttons

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -2,12 +2,19 @@
 import { Link  } from "react-router-dom"
 import { Button, Container , Stack} from "react-bootstrap"
 
-export default function NavBar() {
+export default function NavBar({ amount, max }) {
     return(
         <Container className="my-4">
             <Stack direction="horizontal" gap="2" className="mb-5">
-                <h1 className="me-auto">Budget App</h1>
-                <Button variant="primary">New Transaction</Button>
+                <Link to="/transactions/budget">
+                    <h1 className="me-auto">Budget App</h1>
+                </Link>
+                <Link to="/transactions/new">
+                    <Button variant="primary">New Transaction</Button>
+                </Link>
+                <Link to="/transactions">
+                    <Button variant="primary">View Transactions</Button>  
+                </Link>
             </Stack> 
         </Container>
     )
