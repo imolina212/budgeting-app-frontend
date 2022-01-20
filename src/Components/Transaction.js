f4c1f3d2a964684bf64194754e8209278219a5f1
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-01-20 11:30:41 -0500

    added link to each transaction

--- src/Components/Transaction.js
+++ src/Components/Transaction.js
@@ -1,9 +1,13 @@
-export default function Transaction({transaction, index}) {
+import { Link } from "react-router-dom"
+
+export default function Transaction({transaction, id}) {
     return(
         <tr>
             <td>{transaction.date}</td>
-            <td>{transaction.name}</td>
+            <Link to={`/transactions/${id}`}>
+                <td>{transaction.name}</td>
+            </Link>
             <td>{transaction.amount}</td>
         </tr>
-        );
+    );
 }
