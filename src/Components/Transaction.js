bfe9135f823ac9ff4b5e2c33a7319d91f25a861b
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-01-20 11:36:16 -0500

    removed link underline

--- src/Components/Transaction.js
+++ src/Components/Transaction.js
@@ -4,7 +4,7 @@ export default function Transaction({transaction, id}) {
     return(
         <tr>
             <td>{transaction.date}</td>
-            <Link to={`/transactions/${id}`}>
+            <Link to={`/transactions/${id}`} style={{textDecoration: "none"}}>
                 <td>{transaction.name}</td>
             </Link>
             <td>{transaction.amount}</td>
