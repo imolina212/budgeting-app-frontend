ce17bf295f92895b8f3eaa87ac5908fb2cb2ec14
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-01-19 22:03:01 -0500

    moved BudgetCard to its own page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import NavBar from "./Components/NavBar"
 //Pages
 import Home from "./Pages/Home"
 import Index from "./Pages/Index"
+import New from "./Pages/New"
+import Budget from "./Pages/Budget"
 
 export default function App() {
   return(
@@ -14,6 +16,8 @@ export default function App() {
       <Routes>
         <Route exact path="/" element={<Home />}/>
         <Route exact path="/transactions" element={<Index />}/>
+        <Route exact path="/transactions/new" element={<New />}/>
+        <Route exact path="/transactions/budget" element={<Budget />}/>
       </Routes>
     </div>
   )
