cbce6765ab16487928c7fc9536c7acd373ed0ba7
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-01-20 21:44:31 -0500

    added button divider

--- src/Components/TransactionDetails.js
+++ src/Components/TransactionDetails.js
@@ -24,6 +24,7 @@ export default function TransactionDetails() {
                 <p class="card-text">Category({transaction.category})</p>
                 <Stack direction="horizontal" gap="3">
                     <Button>Back</Button>
+                    <div class="vr"></div>
                     <Button>Edit</Button>
                 </Stack>
             </div>
