6efc8e97a3ef95d6a7482122479b98a9891a4ecd
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-01-20 23:08:30 -0500

    added axios to fetch data

--- src/Components/NewForm.js
+++ src/Components/NewForm.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
@@ -10,6 +11,14 @@ export default function NewForm() {
     amount: 0,
     from: "",
   })
+
+  useEffect(() => {
+    axios.get(`${process.env.REACT_APP_API_URL}/transactions/${id}`)
+      .then((response) => {
+        setTransaction(response.data)
+    })
+    .catch((err) => console.log('catch', err))
+  }, [])
   
   return(
     <form></form>
