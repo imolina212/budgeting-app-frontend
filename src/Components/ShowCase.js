9b01c69109ffff055a28f07e06921781145bd80e
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-01-23 13:24:25 -0500

    added dummy text + contained img

--- src/Components/ShowCase.js
+++ src/Components/ShowCase.js
@@ -4,10 +4,13 @@ export default function ShowCase() {
     return (
         <section className="bg-light text-dark p-5 text-center">
             <div className="container">
-                <div>
-                    <h1>Manage Your Finances<span className="text-success"> Efficiently</span ></h1>
+                <div className="d-flex">
+                    <div>
+                        <h1>Manage Your Finances<span className="text-success"> Efficiently</span></h1>
+                        <p>Lorem ipsum dolor sit amet consectetur adipiscing elit dictum, porttitor curae erat feugiat per duis id posuere, maecenas proin aliquam viverra bibendum enim vestibulum. Litora dis libero accumsan ac id nec suspendisse habitasse fames, aenean hendrerit nullam bibendum sem cubilia nostra.</p>
+                    </div>
                 </div>
-                <img src={make_it_rain} alt=""/>
+                <img className="img-fluid" src={make_it_rain} alt=""/>
             </div>
         </section>
     )
